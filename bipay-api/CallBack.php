764bafb6cf15ff622cd66e07cb85d9a853ab86d7
chengmingfeng <993782244@qq.com>
2020-04-18 21:02:56 +0800

    增加交易回调

---
+++ bipay-api/CallBack.php
@@ -0,0 +1,35 @@
+<?php
+
+//回调示例
+$call_back_data = array(
+    'timestamp' => (string) $_POST['timestamp'],
+    'nonce' => (string) $_POST['nonce'],
+    'sign' => (string) $_POST['sign'],
+    'body' => (string) $_POST['body'],
+);
+
+file_put_contents("call_back_data.txt", "\n" . date('Y-m-d H:i:s') . json_encode($call_back_data) . "\n", FILE_APPEND);
+
+$sign = md5($call_back_data['body'] . CLIENTCONFIG['api_key'] . $call_back_data['nonce'] . $call_back_data['timestamp']);
+
+if ($call_back_data['sign'] == $sign) {
+    $body = json_decode($call_back_data['body']);
+
+    //$body->tradeType 0待审核 1审核成功 2审核驳回 3交易成功 4交易失败
+    if($body->tradeType == 3){
+
+        //业务处理完成返回 服务器不再回调
+        return "success";
+    }
+
+    if ($body->tradeType == 1) {
+        //充币回调
+    } elseif ($body->tradeType == 2) {
+        //提币回调
+    }
+
+    file_put_contents("call_back_data.txt", "\n" . date('Y-m-d H:i:s') . "\n sign success: \n" . $sign . "\n" . $call_back_data['sign'] . "\n", FILE_APPEND);
+} else {
+    echo 'sign error';
+    file_put_contents("call_back_data.txt", "\n" . date('Y-m-d H:i:s') . "\n sign error: \n" . $sign . "\n" . $call_back_data['sign'] . "\n", FILE_APPEND);
+}
